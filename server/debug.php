893c97f92a23bacee6c1ec508e6a3a655550e116
AncientSion <rolandthegunslinger@web.de>
2017-05-15 18:35:08 +0200

    3
    3

--- server/debug.php
+++ server/debug.php
@@ -3,7 +3,7 @@
 class Debug {
 	
 	static function log($string){
-        file_put_contents('/tmp/fire.log', $msg, FILE_APPEND);
+        file_put_contents('/tmp/fire.log', $string, FILE_APPEND);
 		//file_put_contents('fire.log', $string."\n", FILE_APPEND);
 	}
 }
