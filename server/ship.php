c8f5063f00ca0b917bf6fb01d898e0a7225b3950
AncientSion <rolandthegunslinger@web.de>
2017-07-06 16:49:20 +0200

    2
    2

--- server/ship.php
+++ server/ship.php
@@ -68,6 +68,7 @@ class Ship {
 		$this->baseHitChance = ceil(pow($this->mass, 1/3)*5);
 		$this->baseTurnCost = round(pow($this->mass, 1.25)/20000, 2);
 		$this->baseTurnDelay = round(pow($this->mass, 0.5)/25, 2);
+		$this->baseImpulseCost = round(pow($this->mass, 1.2)/500, 2);
 	}
 
 	public function hidePowers($turn){
