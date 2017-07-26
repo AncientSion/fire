e518e765c1a27db3387f294352ffe10ab4a5ded7
AncientSion <rolandthegunslinger@web.de>
2017-07-26 22:08:37 +0200

    2
    2

--- server/dbManager.php
+++ server/dbManager.php
@@ -9,7 +9,7 @@ class DBManager {
 
 		if ($this->connection === null){
 			$user = "aatu"; $pass = "Kiiski";
-			$user = "root"; $pass = "147147";
+		//	$user = "root"; $pass = "147147";
 			$this->connection = new PDO("mysql:host=localhost;dbname=spacecombat",$user,$pass);
 			//$this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			//$this->connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
