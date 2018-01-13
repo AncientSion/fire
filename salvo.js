e4c6824f1667b2fa568d356d8bc60e011436e7da
AncientSion <rolandthegunslinger@web.de>
2018-01-14 00:19:33 +0100

    salvo image

--- salvo.js
+++ salvo.js
@@ -263,7 +263,12 @@ Salvo.prototype.supplyAttachDiv = function(div){
 
 	for (var j = 0; j < this.structures.length; j++){
 		if (this.structures[j].destroyed || this.structures[j].disabled){continue;}
-		attachDiv.append($("<div>").append($("<img>").css("width", 34).css("height", 34).attr("src", graphics.images[this.structures[j].name.toLowerCase()].src)));
+		attachDiv.append($("<div>")
+			.append($(this.structures[j].getBaseImage().cloneNode(true))
+				.css("width", 34)
+				.css("height", 34)
+			)
+		)
 	}
 
 	div.append(attachDiv);
