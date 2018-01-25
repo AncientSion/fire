a820c9315c9ce65d6412029bc50cb2c91820932e
AncientSion <rolandthegunslinger@web.de>
2018-01-25 14:44:04 +0100

    db debug

--- postGameData.php
+++ postGameData.php
@@ -4,6 +4,96 @@
 $manager;
 $dbManager;
 
-echo "done";
+if (isset($_POST["type"])) {
+	$dbManager = DBManager::app();
+
+	if ($_POST["type"] == "chat"){
+		$dbManager->insertChatMsg($_POST);
+		return true;
+	}
+
+	$manager = new Manager($_POST["userid"], $_POST["gameid"]);
+
+	if ($_POST["type"] == "joinGame") {
+		Debug::log("joinGame success");
+		if ($dbManager->createPlayerStatus($_POST["userid"], $_POST["gameid"], 0, -1, "joined")) {
+			Debug::log("joinGame success");
+		}
+		else {
+			echo "joinGame fail";
+		}
+	}
+	else if ($_POST["type"] == "leaveGame") {
+		echo $dbManager->leaveGame($_POST["userid"], $_POST["gameid"]);
+	}
+	else if ($_POST["type"] == "buyInitialFleet") {
+		$rem = $manager->validateFleetCost($_POST["ships"]);
+		if ($rem >= 0){
+			if ($dbManager->processInitialBuy($_POST["userid"], $_POST["gameid"], $_POST["ships"], $rem, $_POST["faction"])){
+				if ($dbManager->gameIsReady($_POST["gameid"])) {
+					if ($dbManager->startGame($_POST["gameid"])) {
+						header("Location: game.php?gameid=".$_POST["gameid"]);
+					}
+				}
+				else {
+					header("Location: lobby.php");
+				}
+			}
+		}
+		else echo "Invalid Fleet";
+	}
+	else if ($_POST["type"] == "deployment"){
+		if (isset($_POST["initial"])){
+			$dbManager->deployShipsDB($_POST["gameid"], $_POST["initial"]);
+		}
+		if (isset($_POST["reinforce"])){
+			$dbManager->requestShipsDB($_POST["userid"], $_POST["gameid"], $manager->turn, $_POST["reinforce"]);
+		}
+		if (isset($_POST["deployedFlights"])){
+			$dbManager->deployFlightsDB($_POST["userid"], $_POST["gameid"], $_POST["deployedFlights"]);
+		}
+		if (isset($_POST["missions"])){
+			$dbManager->insertMissions($_POST["missions"]);
+		}
+		if (isset($_POST["powers"])){
+			$dbManager->insertPowers($_POST["gameid"], $_POST["turn"], $_POST["powers"]);
+		}
+		if (isset($_POST["fireOrders"])){
+			$dbManager->insertClientFireOrders($_POST["gameid"], $_POST["turn"], $_POST["fireOrders"]);
+		}
+		if (isset($_POST["ew"])){
+			$dbManager->insertEW($_POST["ew"]);
+		}
+		//return;
+		$dbManager->setPlayerStatus($_POST["userid"], $_POST["gameid"], $_POST["turn"], $_POST["phase"], "ready");
+	}
+	else if ($_POST["type"] == "movement"){;
+		if ($dbManager->insertClientActions($_POST["ships"])){
+			if ($dbManager->setPlayerStatus($_POST["userid"], $_POST["gameid"], $_POST["turn"], $_POST["phase"], "ready")){
+				//echo "movement success";
+			}
+		} else echo "movement fail";
+	}
+	else if ($_POST["type"] == "firing"){
+		if ($dbManager->insertClientFireOrders($_POST["gameid"], $_POST["turn"], $_POST["fireOrders"])){
+			//return;
+			if ($dbManager->setPlayerStatus($_POST["userid"], $_POST["gameid"], $_POST["turn"], $_POST["phase"], "ready")){
+				echo "firing success";
+			}
+		}
+		else {
+			echo "firing fail";
+		}
+	}
+	else if ($_POST["type"] == "damageControl"){
+		if (sizeof($_POST["data"])){$dbManager->jumpOutUnits($_POST["data"])});
+		if ($dbManager->setPlayerStatus($_POST["userid"], $_POST["gameid"], $_POST["turn"], $_POST["phase"], "ready")){
+			echo "damageControl success";
+		}
+	}
+}
+else {
+	echo "no data to be found";
+}
 
 ?>
