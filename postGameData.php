d891f6a457ff112f29528c8438f6708216c4aa74
AncientSion <rolandthegunslinger@web.de>
2018-01-25 14:48:03 +0100

    db debug

--- postGameData.php
+++ postGameData.php
@@ -26,22 +26,6 @@ if (isset($_POST["type"])) {
 	else if ($_POST["type"] == "leaveGame") {
 		echo $dbManager->leaveGame($_POST["userid"], $_POST["gameid"]);
 	}
-	else if ($_POST["type"] == "buyInitialFleet") {
-		$rem = $manager->validateFleetCost($_POST["ships"]);
-		if ($rem >= 0){
-			if ($dbManager->processInitialBuy($_POST["userid"], $_POST["gameid"], $_POST["ships"], $rem, $_POST["faction"])){
-				if ($dbManager->gameIsReady($_POST["gameid"])) {
-					if ($dbManager->startGame($_POST["gameid"])) {
-						header("Location: game.php?gameid=".$_POST["gameid"]);
-					}
-				}
-				else {
-					header("Location: lobby.php");
-				}
-			}
-		}
-		else echo "Invalid Fleet";
-	}
 }
 else {
 	echo "no data to be found";
