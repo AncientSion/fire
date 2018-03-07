eb754c0d754f821afe9f576973fb26c3830e66b8
AncientSion <rolandthegunslinger@web.de>
2018-03-07 11:02:20 +0100

    lobby

--- lobby.php
+++ lobby.php
@@ -57,6 +57,7 @@ if (isset($_SESSION["userid"])){
 		$myGamesElement .= "</tr>";
 		
 		foreach ($myGames as $game){
+			if ($game["turn"] == -1){continue;}
 			$phase = getPhaseString($game["phase"]);
 			
 			$myGamesElement .= "<tr id=".$game['id'].">";
