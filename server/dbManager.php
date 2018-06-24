540c8ff04dd465a504f0de2c392e89f4eba6de65
AncientSion <rolandthegunslinger@web.de>
2018-06-24 13:25:53 +0200

    1
    1

--- server/dbManager.php
+++ server/dbManager.php
@@ -372,9 +372,9 @@
 			$id;
 
 			if (class_exists("Hyperion")){
-				Debug::log("does!")
+				Debug::log("does!");
 			} else Debug::log("does n ot!");
-			
+
 			for ($i = 0; $i < sizeof($units); $i++){
 				//Debug::log("checking!");
 				if ($units[$i]["command"]){
