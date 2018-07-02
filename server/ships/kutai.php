d2753be569988b86ba3d02d046c8fc9de2680fc2
AncientSion <rolandthegunslinger@web.de>
2018-07-02 18:48:15 +0200

    1
    1

---
+++ server/ships/kutai.php
@@ -0,0 +1,49 @@
+<?php
+
+class Kutai extends Medium {
+	public $name = "Kutai";
+	public $display = "Kutai";
+	public $faction = "Centauri Republic";
+	public $size =  50;
+	public static $value = 440;
+	public $profile = array(0.93, 1.07);
+	public $mass = 3800;
+
+	public $integrity = 650;
+	public $vitalHP = 80;
+	public $ep = 70;
+	public $ew = 675;
+
+	function __construct($data = false){
+        parent::__construct($data);
+	}
+
+	public function addStructures(){
+		$structs = array();
+
+		$front = new Structure($this->getId(), $this->id, 300, 60, 400, 16, 4);
+		$front->systems[] = new LightParticle($this->getId(), $this->id, 240, 60);
+		$front->systems[] = new MediumParticle($this->getId(), $this->id, 300, 60);
+		$front->systems[] = new MediumParticle($this->getId(), $this->id, 300, 60);
+		$front->systems[] = new LightParticle($this->getId(), $this->id, 300, 120);
+		$structs[] = $front;
+
+		$right = new Structure($this->getId(), $this->id, 60, 180, 325, 15, 1);
+		$right->systems[] = new LightPlasma($this->getId(), $this->id, 0, 180);
+		$right->systems[] = new LightPlasma($this->getId(), $this->id, 0, 180);
+		$right->systems[] = new LightPlasma($this->getId(), $this->id, 0, 180);
+		$structs[] = $right;
+
+		$left = new Structure($this->getId(), $this->id, 180, 300, 325, 15, 1);
+		$left->systems[] = new LightPlasma($this->getId(), $this->id, 180, 360);
+		$left->systems[] = new LightPlasma($this->getId(), $this->id, 180, 360);
+		$left->systems[] = new LightPlasma($this->getId(), $this->id, 180, 360);
+		$structs[] = $left;
+
+		for ($i = 0; $i < sizeof($structs); $i++){
+			$this->structures[] = $structs[$i];
+		}
+	}
+}
+
+?>
---
+++ server/ships/vorchan.php
@@ -0,0 +1,87 @@
+<?php
+
+class Vorchan extends Light {
+	public $name = "Vorchan";
+	public $display = "Vorchan";
+	public $role = "Attack Frigate";
+	public $faction = "Centauri Republic";
+	public static $value = 300;
+	public $mass = 1250;
+	public $integrity = 400;
+	
+	public $ep = 120;
+	public $ew = 525;
+	public $power = 2;
+	public $negation = 11;
+
+	function __construct($id, $userid){
+		parent::__construct($id, $userid);
+	}
+
+	public function addStructures(){
+		$structs = array();
+
+		$front = new Section(300, 60);
+		$front->systems[] = new MissileLauncher($this->getId(), $this->parentId, 300, 60, array(array("Javelin", 4, 2), array("Hasta", 8, 4)));
+		$structs[] = $front;
+
+		$right = new Section(60, 180);
+		$right->systems[] = new LightPlasma($this->getId(), $this->parentId, 315, 45);
+		$structs[] = $right;
+
+		$left = new Section(180, 300);
+		$left->systems[] = new LightPlasma($this->getId(), $this->parentId, 315, 45);
+		$structs[] = $left;
+
+		for ($i = 0; $i < sizeof($structs); $i++){
+			$this->structures[] = $structs[$i];
+		}
+	}
+
+	static function getKit($faction){
+		return array(
+			"id" => 0,
+			"name" => "",
+			"cost" => static::$value,
+			"gameid" => 0,
+			"userid" => 0,
+			"upgrades" => 
+			array(
+				array(
+					"active" => 0,
+					"chance" => 80,
+					"cost" => 60,
+					"name" => "Standard Outfit",
+					"units" => array(),
+					"loads" => 
+					array(
+						array(
+							"systemid" => 7,
+							"display" => "Front Missile Launcher",
+							"name" => "Hasta",
+							"amount" => 6
+						)
+					)
+				),
+				array(
+					"active" => 0,
+					"chance" => 80,
+					"cost" => 60,
+					"name" => "Standard Outfit",
+					"units" => array(),
+					"loads" => 
+					array(
+						array(
+							"systemid" => 7,
+							"display" => "Front Missile Launcher",
+							"name" => "Hasta",
+							"amount" => 6
+						)
+					)
+				)
+			)
+		);
+	}
+}
+
+?>
