83c984d1780e6ead3f11496a5b59f0bb6edaf8ce
AncientSion <rolandthegunslinger@web.de>
2018-09-22 10:28:07 +0200

    1
    1

--- game.js
+++ game.js
@@ -2070,6 +2070,8 @@ function Game(data){
 		for (var i = 0; i < this.ships.length; i++){
 			if (!this.ships[i].deployed){continue;}
 			for (var j = 0; j < this.ships[i].actions.length; j++){
+				if (!this.ships[i].actions[j].forced){continue;}
+				
 				if (this.ships[i].actions[j].forced && this.actions[i].type != "jumpOut"){
 					need = 1;
 					this.ships[i].toAnimate = 1;
