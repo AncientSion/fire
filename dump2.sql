57779e2885836922644661dd70e9aab34aa114cf
AncientSion <rolandthegunslinger@web.de>
2018-10-22 09:36:56 +0200

    db.dump

---
+++ dump2.sql
@@ -0,0 +1,495 @@
+-- MySQL dump 10.16  Distrib 10.1.16-MariaDB, for Win32 (AMD64)
+--
+-- Host: localhost    Database: spacecombat
+-- ------------------------------------------------------
+-- Server version	10.1.16-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `actions`
+--
+
+DROP TABLE IF EXISTS `actions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `actions` (
+  `id` int(3) NOT NULL AUTO_INCREMENT,
+  `shipid` int(3) DEFAULT NULL,
+  `turn` int(3) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `forced` tinyint(4) NOT NULL DEFAULT '0',
+  `dist` int(4) DEFAULT NULL,
+  `x` int(4) DEFAULT NULL,
+  `y` int(4) DEFAULT NULL,
+  `a` decimal(5,2) NOT NULL DEFAULT '0.00',
+  `cost` int(4) DEFAULT NULL,
+  `delay` int(4) DEFAULT NULL,
+  `costmod` float DEFAULT NULL,
+  `resolved` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `actions`
+--
+
+LOCK TABLES `actions` WRITE;
+/*!40000 ALTER TABLE `actions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `actions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `chat`
+--
+
+DROP TABLE IF EXISTS `chat`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `chat` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) DEFAULT NULL,
+  `userid` int(3) DEFAULT NULL,
+  `msg` varchar(255) DEFAULT NULL,
+  `time` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `chat`
+--
+
+LOCK TABLES `chat` WRITE;
+/*!40000 ALTER TABLE `chat` DISABLE KEYS */;
+/*!40000 ALTER TABLE `chat` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `damages`
+--
+
+DROP TABLE IF EXISTS `damages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `damages` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `fireid` int(5) DEFAULT NULL,
+  `gameid` int(5) DEFAULT NULL,
+  `shipid` int(5) DEFAULT NULL,
+  `structureid` int(5) DEFAULT NULL,
+  `systemid` int(3) DEFAULT NULL,
+  `turn` int(3) DEFAULT NULL,
+  `roll` int(3) DEFAULT NULL,
+  `type` varchar(20) DEFAULT NULL,
+  `totalDmg` int(5) DEFAULT NULL,
+  `shieldDmg` int(5) DEFAULT NULL,
+  `armourDmg` int(3) DEFAULT NULL,
+  `systemDmg` int(5) NOT NULL DEFAULT '0',
+  `hullDmg` int(5) NOT NULL DEFAULT '0',
+  `emDmg` int(3) DEFAULT '0',
+  `negation` int(4) DEFAULT NULL,
+  `destroyed` tinyint(1) DEFAULT NULL,
+  `notes` varchar(255) DEFAULT NULL,
+  `new` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `damages`
+--
+
+LOCK TABLES `damages` WRITE;
+/*!40000 ALTER TABLE `damages` DISABLE KEYS */;
+/*!40000 ALTER TABLE `damages` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `fireorders`
+--
+
+DROP TABLE IF EXISTS `fireorders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `fireorders` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `gameid` int(3) DEFAULT '0',
+  `turn` int(3) DEFAULT '0',
+  `shooterid` int(5) DEFAULT '0',
+  `targetid` int(5) DEFAULT '0',
+  `x` int(3) DEFAULT '0',
+  `y` int(3) DEFAULT '0',
+  `weaponid` int(5) DEFAULT '0',
+  `shots` int(3) NOT NULL DEFAULT '0',
+  `req` int(3) NOT NULL DEFAULT '0',
+  `notes` varchar(255) NOT NULL DEFAULT '',
+  `hits` int(3) NOT NULL DEFAULT '0',
+  `resolved` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `fireorders`
+--
+
+LOCK TABLES `fireorders` WRITE;
+/*!40000 ALTER TABLE `fireorders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `fireorders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `games`
+--
+
+DROP TABLE IF EXISTS `games`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `games` (
+  `id` int(3) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `turn` int(3) DEFAULT NULL,
+  `phase` int(3) DEFAULT NULL,
+  `pv` int(5) DEFAULT NULL,
+  `reinforce` int(5) DEFAULT NULL,
+  `reinforceTurn` int(2) DEFAULT '11',
+  `reinforceETA` int(2) DEFAULT '3',
+  `reinforceAmount` int(2) NOT NULL DEFAULT '10',
+  `focusMod` int(3) DEFAULT '100',
+  `obstaclesAmount` int(3) NOT NULL DEFAULT '0',
+  `obstaclesSizeMin` int(3) NOT NULL DEFAULT '0',
+  `obstaclesSizeMax` int(3) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `games`
+--
+
+LOCK TABLES `games` WRITE;
+/*!40000 ALTER TABLE `games` DISABLE KEYS */;
+/*!40000 ALTER TABLE `games` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `globals`
+--
+
+DROP TABLE IF EXISTS `globals`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `globals` (
+  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `playerstatusid` int(4) DEFAULT '0',
+  `unitid` int(3) NOT NULL DEFAULT '0',
+  `turn` int(4) DEFAULT '0',
+  `type` varchar(20) DEFAULT '',
+  `scope` int(11) NOT NULL DEFAULT '0',
+  `value` decimal(5,2) DEFAULT '0.00',
+  `notes` varchar(20) NOT NULL DEFAULT '',
+  `text` varchar(50) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `globals`
+--
+
+LOCK TABLES `globals` WRITE;
+/*!40000 ALTER TABLE `globals` DISABLE KEYS */;
+/*!40000 ALTER TABLE `globals` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `loads`
+--
+
+DROP TABLE IF EXISTS `loads`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `loads` (
+  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `shipid` int(5) DEFAULT NULL,
+  `systemid` int(3) DEFAULT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `amount` int(3) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `loads`
+--
+
+LOCK TABLES `loads` WRITE;
+/*!40000 ALTER TABLE `loads` DISABLE KEYS */;
+/*!40000 ALTER TABLE `loads` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `missions`
+--
+
+DROP TABLE IF EXISTS `missions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `missions` (
+  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `unitid` int(4) NOT NULL DEFAULT '0',
+  `type` varchar(20) NOT NULL DEFAULT '1',
+  `turn` int(1) NOT NULL DEFAULT '0',
+  `targetid` int(4) NOT NULL DEFAULT '0',
+  `x` int(4) NOT NULL DEFAULT '0',
+  `y` int(4) NOT NULL DEFAULT '0',
+  `arrived` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `missions`
+--
+
+LOCK TABLES `missions` WRITE;
+/*!40000 ALTER TABLE `missions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `missions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `playerstatus`
+--
+
+DROP TABLE IF EXISTS `playerstatus`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `playerstatus` (
+  `id` int(3) NOT NULL AUTO_INCREMENT,
+  `userid` int(3) DEFAULT NULL,
+  `gameid` int(3) DEFAULT NULL,
+  `turn` int(3) DEFAULT NULL,
+  `phase` int(3) DEFAULT NULL,
+  `faction` varchar(20) DEFAULT NULL,
+  `morale` int(4) NOT NULL DEFAULT '0',
+  `value` int(5) DEFAULT NULL,
+  `maxFocus` int(4) DEFAULT '0',
+  `gainFocus` int(4) DEFAULT '0',
+  `curFocus` int(4) DEFAULT '0',
+  `status` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `playerstatus`
+--
+
+LOCK TABLES `playerstatus` WRITE;
+/*!40000 ALTER TABLE `playerstatus` DISABLE KEYS */;
+/*!40000 ALTER TABLE `playerstatus` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `powers`
+--
+
+DROP TABLE IF EXISTS `powers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `powers` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `unitid` int(5) DEFAULT NULL,
+  `systemid` int(5) DEFAULT NULL,
+  `turn` int(2) DEFAULT NULL,
+  `type` varchar(10) DEFAULT NULL,
+  `cost` int(3) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `powers`
+--
+
+LOCK TABLES `powers` WRITE;
+/*!40000 ALTER TABLE `powers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `powers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sensors`
+--
+
+DROP TABLE IF EXISTS `sensors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sensors` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `unitid` int(5) DEFAULT NULL,
+  `systemid` int(5) DEFAULT NULL,
+  `turn` int(3) DEFAULT NULL,
+  `angle` decimal(5,2) DEFAULT NULL,
+  `dist` int(4) DEFAULT NULL,
+  `type` int(3) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sensors`
+--
+
+LOCK TABLES `sensors` WRITE;
+/*!40000 ALTER TABLE `sensors` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sensors` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `subunits`
+--
+
+DROP TABLE IF EXISTS `subunits`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `subunits` (
+  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `unitid` int(4) DEFAULT NULL,
+  `amount` int(2) DEFAULT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `subunits`
+--
+
+LOCK TABLES `subunits` WRITE;
+/*!40000 ALTER TABLE `subunits` DISABLE KEYS */;
+/*!40000 ALTER TABLE `subunits` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `systemcrits`
+--
+
+DROP TABLE IF EXISTS `systemcrits`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `systemcrits` (
+  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `shipid` int(4) DEFAULT NULL,
+  `systemid` int(4) DEFAULT NULL,
+  `turn` int(3) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `duration` int(3) DEFAULT NULL,
+  `value` decimal(5,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `systemcrits`
+--
+
+LOCK TABLES `systemcrits` WRITE;
+/*!40000 ALTER TABLE `systemcrits` DISABLE KEYS */;
+/*!40000 ALTER TABLE `systemcrits` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `units`
+--
+
+DROP TABLE IF EXISTS `units`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `units` (
+  `id` int(3) NOT NULL AUTO_INCREMENT,
+  `gameid` int(3) DEFAULT '0',
+  `userid` int(3) DEFAULT '0',
+  `type` int(1) NOT NULL DEFAULT '0',
+  `name` varchar(255) DEFAULT '',
+  `callsign` varchar(40) NOT NULL DEFAULT '',
+  `totalCost` int(4) NOT NULL DEFAULT '0',
+  `moraleCost` int(4) NOT NULL DEFAULT '0',
+  `status` varchar(255) DEFAULT '',
+  `command` tinyint(4) DEFAULT '0',
+  `available` int(3) DEFAULT '0',
+  `withdraw` int(2) NOT NULL DEFAULT '0',
+  `manual` tinyint(4) NOT NULL DEFAULT '0',
+  `destroyed` tinyint(1) DEFAULT '0',
+  `x` int(4) DEFAULT '0',
+  `y` int(4) DEFAULT '0',
+  `facing` decimal(5,2) NOT NULL DEFAULT '0.00',
+  `delay` int(4) NOT NULL DEFAULT '0',
+  `thrust` int(3) DEFAULT '0',
+  `rolling` int(2) DEFAULT '0',
+  `rolled` int(2) DEFAULT '0',
+  `flipped` int(2) DEFAULT '0',
+  `turn` int(1) DEFAULT '0',
+  `phase` int(1) DEFAULT '0',
+  `focus` tinyint(4) DEFAULT '0',
+  `notes` varchar(50) DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `units`
+--
+
+LOCK TABLES `units` WRITE;
+/*!40000 ALTER TABLE `units` DISABLE KEYS */;
+/*!40000 ALTER TABLE `units` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(3) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `access` int(3) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Chris','1',0),(2,'1','1',0);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-10-22  9:36:23
